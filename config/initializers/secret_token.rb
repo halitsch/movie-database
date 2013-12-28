73daeb938c19f373f28eea992ddf8af127bd7769
Petr Haloun <haloun@itlab.cz>
2013-12-28 15:25:57 +0100

    initial commit - empty ruby on rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviedatabase::Application.config.secret_key_base = '07be7ade372420787a182d8c7e78e65bb3ac249e329701d56d1badc0feee74d9ab3b44094d25667018269876178353e5efaddb8f6e02e97fd875e936b235208c'

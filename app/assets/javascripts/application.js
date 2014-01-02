8963ff03603ba44749135b3f8e1fed79402aeafb
Petr Haloun <haloun@itlab.cz>
2014-01-02 15:44:31 +0100

    javascript document ready fix (because turbolinks in rails)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var ready;
+ready = function() {
+
+	$("table").tablesorter();
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
